225ed80ed6d20c3aaf0b70687c573af2476e0968
Rusen Yasar <rusen138@gmail.com>
2024-08-01 12:59:34 +0200

    Attempt to load nltk resources manually

--- src/Convert_BUSSE.py
+++ src/Convert_BUSSE.py
@@ -52,7 +52,8 @@ Warengruppen_dict = {
 }
 
 from src.stopwords_de import stopwords
-nltk.data.path.append("data/nltk_data")
+nltk.data.load("data/german.pickle")
+nltk.data.load("data/german")
 
 def normalize_document(doc):
     # fix contractions
